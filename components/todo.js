35771e9ba7d6fff18c0db5469ea2c0367f1bc1e6
Hector Ramon <hlramon93@gmail.com>
2021-01-11 10:41:03 -0400

    Add styles to todo component

--- components/todo.js
+++ components/todo.js
@@ -1,26 +1,40 @@
 function Todo({ todo, onCheckCompleted, updateTodoTitle, deleteTodo }) {
   return (
     <li>
-      <p>
-        <input
-          type="checkbox"
-          checked={todo.completed}
-          onChange={() => {
-            onCheckCompleted(todo.id);
-          }}
-        />
-        <input
-          type="text"
-          defaultValue={todo.title}
-          onBlur={(e) => {
-            updateTodoTitle({
-              id: todo.id,
-              title: e.target.value,
-            });
-          }}
-        />
-        <button onClick={() => deleteTodo(todo.id)}>X</button>
-      </p>
+      <div className="row">
+        <div className="col-sm-1 text-center">
+          <div className="checkbox">
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => {
+                onCheckCompleted(todo.id);
+              }}
+            />
+          </div>
+        </div>
+        <div className="col-sm-8">
+          <input
+            type="text"
+            className="form-control"
+            defaultValue={todo.title}
+            onBlur={(e) => {
+              updateTodoTitle({
+                id: todo.id,
+                title: e.target.value,
+              });
+            }}
+          />
+        </div>
+        <div className="col-sm-3">
+          <button
+            className="btn btn-danger"
+            onClick={() => deleteTodo(todo.id)}
+          >
+            X
+          </button>
+        </div>
+      </div>
     </li>
   );
 }
