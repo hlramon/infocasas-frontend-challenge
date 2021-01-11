7a3069eccb58d390c8a6cb9e6787f50790832cf3
Hector Ramon <hlramon93@gmail.com>
2021-01-11 10:38:57 -0400

    Add bootstrap theme

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,18 @@
 import Head from "next/head";
+import styles from "./layout.module.css";
 
 function Layout({ children }) {
   return (
     <div>
       <Head>
         <title>Personal Task Management</title>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/lumen/bootstrap.min.css"
+        />
       </Head>
-      {children}
+      <div className="container">{children}</div>
     </div>
   );
 }
